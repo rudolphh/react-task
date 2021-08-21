807e3dac3f8103323b14828c56580f0e9e5314b5
Rudy A. Hernandez <rudolpharthur@gmail.com>
2021-08-21 16:04:50 -0700

    final

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,18 @@
+import { useLocation } from "react-router-dom";
 import PropTypes from "prop-types";
 import Button from "./Button";
 
 const Header = ({ title, showAddTaskForm, showAdd }) => {
+  const location = useLocation();
+
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button
+      {location.pathname === '/' && <Button
         color={showAdd ? "red" : "green"}
         text={showAdd ? "Close" : "Add"}
         onClick={showAddTaskForm}
-      />
+      />}
     </header>
   );
 };
