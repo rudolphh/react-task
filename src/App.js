02528034bdf196736c650c30c958cd0c0c1c2f45
Rudy A. Hernandez <rudolpharthur@gmail.com>
2021-08-21 14:30:43 -0700

    update reminder flag on backend

--- src/App.js
+++ src/App.js
@@ -21,6 +21,13 @@ function App() {
     return data;
   };
 
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
+    return data;
+  };
+
+
   const showAddTaskForm = () => {
     setShowAddTask(!showAddTask);
   };
@@ -48,7 +55,19 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+
+    const taskToToggle = await fetchTask(id);
+    const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(updatedTask)
+    });
+
     setTasks(
       tasks.map((task) =>
         task.id === id ? { ...task, reminder: !task.reminder } : task
